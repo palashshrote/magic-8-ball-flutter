fc37529aee6e8f504dca692f4db383c247abc5fc
palashshrote <palash.shrote.58@gmail.com>
2022-11-22 17:31:46 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(Magic8ball());
+}
+
+class Magic8ball extends StatefulWidget {
+  // const Magic8ball({Key key}) : super(key: key);
+
+  @override
+  State<Magic8ball> createState() => _Magic8ballState();
+}
+
+class _Magic8ballState extends State<Magic8ball> {
+  int num = 1;
+  void makeUpdatations() {
+    setState(() {
+      num = Random().nextInt(5) + 1;
+      print('Button pressed and val is $num');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan.shade600,
+        appBar: AppBar(
+          backgroundColor: Colors.cyan.shade800,
+          title: Text('Ask me anything'),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.cyan.shade600,
+                ),
+                onPressed: () {
+                  makeUpdatations();
+                },
+                child: Image(
+                  image: AssetImage('images/ball$num.png'),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
